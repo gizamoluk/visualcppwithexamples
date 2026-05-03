c4c24d9742249b5e76bf769a28b91adf2b571265
gizamolu <gizamoluk@yandex.ru>
2026-05-03 10:36:50 +0500

    добавил пример 7_1

---
+++ ch7/listing7_1.cpp
@@ -0,0 +1,17 @@
+//пример ввода вывода для простых типов данных
+#include "iostream"
+
+using namespace std;
+
+
+void listing6_4() 
+{
+	int i; double x; char a;
+	cout << "Enter an integer ->\t"; cin >> i;
+	cout << "Enter any letter ->\t"; cin >> a;
+	cout << "Enter real value ->\t"; cin >> x;
+	cout << "int\t" << i << endl;
+
+
+	
+}
