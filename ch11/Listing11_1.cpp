084cbb77f40c9f25f5046b56637b1d7a041475c9
gizamolu <gizamoluk@yandex.ru>
2026-05-04 10:56:11 +0500

    добавил главу 11

---
+++ ch11/Listing11_1.cpp
@@ -0,0 +1,17 @@
+//пример ввода вывода для простых типов данных
+#include "iostream"
+
+using namespace std;
+
+
+void listing11_1()
+{
+	int i; double x; char a;
+	cout << "Enter an integer ->\t"; cin >> i;
+	cout << "Enter any letter ->\t"; cin >> a;
+	cout << "Enter real value ->\t"; cin >> x;
+	cout << "int\t" << i << endl;
+
+
+
+}
